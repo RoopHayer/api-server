92757050086d575dda9743e18dca15146bf067b7
Rupinder Hayer <ruphayer@gmail.com>
2021-10-21 23:55:48 -0700

    added channels, messages

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const {Sequelize, DataTypes} = require ('sequelize');
 const FoodModel = require ('./food.js')
 const ClothesModel = require ('./clothes.js')
-
+const channelModel = require('./channel.js');
+const messageModel = require('./message.js');
 
 let DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory';
 
@@ -38,9 +39,15 @@ const sequelizeInstance = new Sequelize(DATABASE_URL, options);
 const foodTable = FoodModel(sequelizeInstance, DataTypes);
 
 const clothesTable = ClothesModel(sequelizeInstance, DataTypes);
+const messages = messageModel(sequelizeInstance, DataTypes);
+const channels = channelModel(sequelizeInstance, DataTypes);
 
+channels.hasMany(messages, { foreignKey: 'channelId', sourceKey: 'id'});
+messages.belongsTo(channels, { foreignKey: 'channelId', targetKey: 'id'});
 module.exports ={
   db: sequelizeInstance,
   food: foodTable,
-  clothes: clothesTable
+  clothes: clothesTable,
+  channels,
+  messages,
 }
